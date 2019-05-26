51c2d6c0a996f8c689af90fb48b6caab84076e8a
yinianhuakai4 <854495176@qq.com>
2019-05-26 20:06:45 +0800

    Initial commit

---
+++ JavaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wzc
+  Date: 2019/5/26
+  Time: 19:23
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>123</title>
+  </head>
+  <body>
+    123213
+  </body>
+</html>
---
+++ Module01/src/com/atguigu/bean/Customer.java
@@ -0,0 +1,21 @@
+package com.atguigu.bean;
+
+import com.sun.org.apache.xpath.internal.SourceTree;
+
+import java.net.SocketPermission;
+
+public class Customer {
+    public static void main(String[] args){
+        System.out.print("1232345");
+
+    }
+    public void metho1(){
+        System.out.println();
+        System.out.println();
+        System.out.println();
+
+    }
+
+
+
+}
